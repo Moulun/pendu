6e0f4408766d2f2b576e9caa937f574dede0bf08
Moulun <Dylan.moulun59@outlook.fr>
2023-09-27 21:52:40 +0200

    first commit

---
+++ mots.js
@@ -0,0 +1,24 @@
+const mots=[
+    ["ECRAN",],
+    ["CLAVIER"],
+    ["SOURIS"],
+    ["CARTE-GRAPHIQUE"],
+    ["ANTIBIOTIQUE"],
+    ["ANTICORPS"],
+    ["ANTIGENES"],
+    ["ANTIBIOGRAMME"],
+    ["BACTERIE"],
+    ["BIOCHIMIE"],
+    ["CAMPYLOBACTER"],
+    ["CANDIDAT"],
+    
+];    
+
+//    MOT                  Indice                                               Thème
+
+const imgs=["imgs/img1.png","imgs/img2.png","imgs/img3.png","imgs/img4.png","imgs/img5.png","imgs/img6.png","imgs/img7.png","imgs/img8.png","imgs/img9.png","imgs/img10.png","imgs/img11.png","imgs/img12.png"];
+
+
+
+
+
